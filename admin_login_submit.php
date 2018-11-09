065151e57f0d76ca87f0b9bce0ece812065c4448
gauravmalick <44890981+gauravmalick@users.noreply.github.com>
2018-11-09 12:59:46 +0530

    Add files via upload

---
+++ admin_login_submit.php
@@ -0,0 +1,20 @@
+<?php 
+	
+
+	
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "obs";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+$email = $_POST['email'];	
+$password = $_POST['password'];
+
+
+?>
